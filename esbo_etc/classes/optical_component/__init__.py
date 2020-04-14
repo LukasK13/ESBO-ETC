3189e6ec675ae2f798b8e7f741f5300968b22efd
LukasK13 <spam.lukas.klass@gmail.com>
2020-04-14 14:29:00 +0200

    Add new modules

--- esbo_etc/classes/optical_component/__init__.py
+++ esbo_etc/classes/optical_component/__init__.py
@@ -1,1 +1,3 @@
 from esbo_etc.classes.optical_component.AOpticalComponent import *
+from esbo_etc.classes.optical_component.Atmosphere import *
+from esbo_etc.classes.optical_component.StrayLight import *
