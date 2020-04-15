8e10a3dcfe5f14c68807221fed55bf83891762c4
LukasK13 <spam.lukas.klass@gmail.com>
2020-04-15 17:14:23 +0200

    Lens added

--- esbo_etc/classes/optical_component/__init__.py
+++ esbo_etc/classes/optical_component/__init__.py
@@ -3,3 +3,4 @@ from esbo_etc.classes.optical_component.Atmosphere import *
 from esbo_etc.classes.optical_component.StrayLight import *
 from esbo_etc.classes.optical_component.AHotOpticalComponent import *
 from esbo_etc.classes.optical_component.Filter import *
+from esbo_etc.classes.optical_component.Lens import *
