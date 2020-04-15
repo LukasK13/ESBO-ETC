68db02c60dca609172c4fc3df5e7bc4a2e7d795e
LukasK13 <spam.lukas.klass@gmail.com>
2020-04-15 17:20:24 +0200

    Mirror added

--- esbo_etc/classes/optical_component/__init__.py
+++ esbo_etc/classes/optical_component/__init__.py
@@ -5,3 +5,4 @@ from esbo_etc.classes.optical_component.AHotOpticalComponent import *
 from esbo_etc.classes.optical_component.Filter import *
 from esbo_etc.classes.optical_component.Lens import *
 from esbo_etc.classes.optical_component.BeamSplitter import *
+from esbo_etc.classes.optical_component.Mirror import *
