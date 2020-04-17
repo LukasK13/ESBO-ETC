1f47f718daebc20e05890bb987dc59018c958ab7
LukasK13 <spam.lukas.klass@gmail.com>
2020-04-17 13:07:28 +0200

    Use relative paths

--- esbo_etc/classes/target/__init__.py
+++ esbo_etc/classes/target/__init__.py
@@ -1,3 +1,3 @@
-from esbo_etc.classes.target.ATarget import *
-from esbo_etc.classes.target.BlackBodyTarget import *
-from esbo_etc.classes.target.FileTarget import *
+from .ATarget import *
+from .BlackBodyTarget import *
+from .FileTarget import *
